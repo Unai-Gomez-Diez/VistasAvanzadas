b0af748cc9aec80bf33be6444b0be12d6707f31d
Unai-Gomez-Diez <unaigomez0807@gmail.com>
2023-11-02 10:59:50 +0100

    avanzando el ex02

--- app/src/main/java/com/ugdgomezdiez/vistasavanzadas/MainActivity.kt
+++ app/src/main/java/com/ugdgomezdiez/vistasavanzadas/MainActivity.kt
@@ -2,10 +2,15 @@ package com.ugdgomezdiez.vistasavanzadas
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Gravity
+import com.google.android.material.appbar.CollapsingToolbarLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.ex02)
+
+
+
     }
 }
